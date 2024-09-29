ea5fbe47f45a9c2f66297bed52107547bce058ab
SamarQaddour <samarqaddour051@gmail.com>
2024-09-29 16:20:37 +0200

    uplaod header and footer component

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+
+export default function Home() {
+  return (
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+       
+      </main>
+     
+ 
+  );
+}
